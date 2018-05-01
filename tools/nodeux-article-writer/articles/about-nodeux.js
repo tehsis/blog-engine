bc783e243dfc5391b3089843092d0b71bd5001b5
Alexis Charytonow <alexis@netjul.com>
2018-04-30 22:39:33 -0700

    about-nodeux, documentation update. (#5)
    * about-nodeux, updated article.
    
    * about-nodeux, updated article, i1

--- tools/nodeux-article-writer/articles/about-nodeux.js
+++ tools/nodeux-article-writer/articles/about-nodeux.js
@@ -7,8 +7,8 @@ function execute() {
     const myArticle = {
         main: {
             id: "about-nodeux",
-            date: "2018-03-13",
-            title: "About Nodeux Blog Engine (in progress)",
+            date: "2018-04-30",
+            title: "About Nodeux Blog Engine",
             hidden_home: false,
             content: {
                 sections: []
@@ -28,25 +28,74 @@ function execute() {
             template: "basic-template",
             fields: {
                 subtitle: "Basic Usage",
-                text: "1. Clone it. <br> 2. Install dependencies and run dev web server. <br> 3. Edit this article, rebuild it and see it in action. <br> 4. Add consistency test. <br> 5. Enjoy."
+                text: "1. Clone it."
             }
         },
         {
             template: "snippet-template",
             fields: {
                 lang: "bash",
-                code: snippetReader.setSnippet('about-nodeux.sh').read()
+                code: "    git clone https://github.com/nodeux/blog-engine.git"
+
             }
         },
         {
             template: "basic-template",
-            fields:
-                {
-                    subtitle: "To be continued ...",
-                    text:
-                        "This article is under development, sync up soon."
-                }
+            fields: {
+                subtitle: "",
+                text: "2. Install dependencies and run dev web server."
+            }
+        },
+        {
+            template: "snippet-template",
+            fields: {
+                lang: "bash",
+                code: "    npm install -g nodemon & npm i & npm run dev"
+            }
+        },
+        {
+            template: "basic-template",
+            fields: {
+                subtitle: "",
+                text: "3. Now, fire up your favorite browser and point it to http://localhost:3000 , then edit the next file tools/nodeux-article-writer/articles/about-nodeux.js (to edit this article) and run the next command to rebuild it."
+            }
+        },
+        {
+            template: "snippet-template",
+            fields: {
+                lang: "bash",
+                code: "    npm run toolbelt/writer"
+            }
+        },
+        {
+            template: "basic-template",
+            fields: {
+                subtitle: "",
+                text: "4. Refresh the webpage and your changes should be applied. Then, add a consistency test to avoid corrupting your blog post in the future. In order to do so, just run the next command."
+            }
+        },
+        {
+            template: "snippet-template",
+            fields: {
+                lang: "bash",
+                code: "    cp tmp/about-nodeux.json test/lib/expected-articles/about-nodeux.json"
+            }
+        },
+        {
+            template: "basic-template",
+            fields: {
+                subtitle: "",
+                text: "5. Now, verify that everything works. Enjoy!"
+            }
+        },
+        {
+            template: "snippet-template",
+            fields: {
+                lang: "bash",
+                code: "    npm test"
+            }
         }
+
     ];
 
     mySections.forEach((s) => {
--- tools/nodeux-article-writer/snippets/about-nodeux.sh
+++
@@ -1,9 +0,0 @@
-1.   git clone https://github.com/nodeux/blog-engine.git
-2a.  npm install -g nodemon
-2b.  npm i & npm run dev
-3a.  go to http://localhost:3000
-3b.  edit tools/nodeux-article-writer/articles/about-nodeux.js
-3c.  npm run toolbelt/writer
-3d.  refresh page
-4.   copy tmp/about-nodeux.js to test/lib/expected-articles/
-4a.  npm test
